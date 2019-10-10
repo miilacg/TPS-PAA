21c783eaec44ccc14742c20905376b1a52c62306
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2019-10-09 21:12:43 -0300

    TP1
    Está funcionando movimenta está funcionando, mas sem chave

--- TP01/labirinto.c
+++ TP01/labirinto.c
@@ -67,108 +67,48 @@ void imprimir(TipoLabirinto *labirinto){
 }
 
 //encontrar a posicao do estudante e depois chamar essa funcao uma unica vez
-int movimenta_estudante(TipoEstudante *estudante, TipoLabirinto *labirinto, TipoAnalise *analise, int x, int y, int caminho[labirinto->linhas][labirinto->colunas]){
+int movimenta_estudante(TipoEstudante *estudante, TipoLabirinto *labirinto, TipoAnalise *analise, int caminho[labirinto->linhas][labirinto->colunas], int x, int y){
+    
 	estudante->pAtual.x = x;
-    estudante->pAtual.y = y;
-    
-    analise->qtdChamadaRecursiva++;
-    analise->nivelMaximo++;
-
-    //caso o estudante chegue na primeira linha retorna 1 (encontrou a solucao) e encerra
-    if (estudante->pAtual.x == 0) {
-    	printf("Linha: %d Coluna: %d\n", x, y);
-        estudante->pFinal.x = x;
-        estudante->pFinal.y = y;
-        caminho[x][y] = 1;
-        return 1;
-    }
-    
-    if ((x > 0) && (y >= 0) && (x < labirinto->linhas) && (y < labirinto->colunas) && (caminho[x][y] == 0) && (labirinto->espaco[x][y] != '2')){
-    	caminho[x][y] = 1; //seta para 1 o caminho
-    	
-    	//Tenta movimentar para cima 
-    	if (labirinto->espaco[x-1][y] == '3'){
-    		if (labirinto->chaves >= 1){
-		        if (movimenta_estudante(estudante, labirinto, analise, x-1, y, caminho)){
-		        	printf("Linha: %d Coluna: %d\n", x, y);
-        			analise->qtdMovimento++;
-		        	labirinto->chaves--;
-					return 1;
-				}
-			}
-		}			
-		if (labirinto->espaco[x-1][y] == '1'){
-			if (movimenta_estudante(estudante, labirinto, analise, x-1, y, caminho)){
-				printf("Linha: %d Coluna: %d\n", x, y);
-        		analise->qtdMovimento++;
-				return 1;
-			}	
-		}
-
-		//Tenta movimentar para esquerda
-		if (labirinto->espaco[x][y-1] == '3'){
-    		if (labirinto->chaves >= 1){
-		        if (movimenta_estudante(estudante, labirinto, analise, x, y-1, caminho)){
-		        	printf("Linha: %d Coluna: %d\n", x, y);
-        			analise->qtdMovimento++;
-		        	labirinto->chaves--;
-					return 1;
-				}
-			}
-		}			
-		if (labirinto->espaco[x][y-1] == '1'){
-			if (movimenta_estudante(estudante, labirinto, analise, x, y-1, caminho)){
-				printf("Linha: %d Coluna: %d\n", x, y);
-        		analise->qtdMovimento++;
-				return 1;
-			}	
-		}
+	estudante->pAtual.y = y;
+	
+	if (estudante->pAtual.x == 0 && labirinto->espaco[x][y] != '2'){
+		estudante->pFinal.x = x;
+		estudante->pFinal.y = y;
 		
-		//Tenta movimentar para direita
-		if (labirinto->espaco[x][y+1] == '3'){
-    		if (labirinto->chaves >= 1){ 
-		        if (movimenta_estudante(estudante, labirinto, analise, x, y+1, caminho)){
-		        	printf("Linha: %d Coluna: %d\n", x, y);
-        			analise->qtdMovimento++;
-		        	labirinto->chaves--;
-					return 1;
-				}	
-			}
-		}
-		if (labirinto->espaco[x][y+1] == '1'){
-			if (movimenta_estudante(estudante, labirinto, analise, x, y+1, caminho)){
-				printf("Linha: %d Coluna: %d\n", x, y);
-        		analise->qtdMovimento++;
-				return 1;
-			}
-		}
-		
-		//Tenta movimentar para baixo	
-		if (labirinto->espaco[x+1][y] == '3'){
-    		if (labirinto->chaves >= 1){
-		        if (movimenta_estudante(estudante, labirinto, analise, x+1, y, caminho)){
-		        	printf("Linha: %d Coluna: %d\n", x, y);
-        			analise->qtdMovimento++;
-		        	labirinto->chaves--;
-					return 1;
-				}
-			}
-		}
-		if (labirinto->espaco[x+1][y] == '1'){
-			if (movimenta_estudante(estudante, labirinto, analise, x+1, y, caminho)){
-				printf("Linha: %d Coluna: %d\n", x, y);
-        		analise->qtdMovimento++;
-				return 1;
-			}
-		}
-		
-        // Memoriza o caminho e ativa o backtracking quando a tentativa de caminhar falha. 
-        caminho[x][y] = 0;
-        analise->nivelMaximo = 0;
-        return 0;		
+		//printf("Linhas: %d Coluna: %d\n", x, y);
+		return 1;
 	}
-	return 0;  
-} 
+	 
+    //verifica se o estudante ira se movimentar para um espaco dentro do labirinto e que nao seja parede
+    if ((x >= 0) && (x < labirinto->linhas) && (y >= 0) && (y < labirinto->colunas) && (labirinto->espaco[x][y] != '2') && (caminho[x][y] == 0)){
+    	caminho[x][y] = 1;    	
+    	analise->qtdMovimento++;
+    	
+		//tenta movimentar para cima
+		if (movimenta_estudante(estudante, labirinto, analise, caminho, x - 1, y)){
+			printf("Linha: %d Coluna: %d\n", x, y);
+			return 1;
+		}
+		//tenta movimentar para direita
+		if (movimenta_estudante(estudante, labirinto, analise, caminho, x, y + 1)){
+			printf("Linha: %d Coluna: %d\n", x, y);		
+			return 1;
+		}
+		//tenta movimentar para esquerda
+		if (movimenta_estudante(estudante, labirinto, analise, caminho, x, y - 1)){
+			printf("Linha: %d Coluna: %d\n", x, y);
+			return 1;
+		}
+		//tenta movimentar para baixo
+		if (movimenta_estudante(estudante, labirinto, analise, caminho, x + 1, y)){
+			printf("Linha: %d Coluna: %d\n", x, y);
+			return 1;
+		}
+		return 0;
+	}	
+	return 0;	
+}
 
 int inicializacoes(TipoLabirinto *labirinto, TipoEstudante *estudante, TipoAnalise *analise){
     int i, j, x, y;
@@ -185,7 +125,7 @@ int inicializacoes(TipoLabirinto *labirinto, TipoEstudante *estudante, TipoAnali
                 estudante->pFinal.x = 0;
                 estudante->pFinal.y = 0;
                 x = estudante->pAtual.x;
-                y = estudante->pAtual.y;
+                y = estudante->pAtual.y; 
             }
         }
     }
@@ -203,20 +143,9 @@ int inicializacoes(TipoLabirinto *labirinto, TipoEstudante *estudante, TipoAnali
 	analise->nivelMaximo = -1; //inicializa com -1 pois a primeira chamada nao e recursiva
     
     // Verifica se o problema tem solucao
-    return movimenta_estudante(estudante, labirinto, analise, x, y, caminho);
-}
-
-void imprimirSolucao(TipoLabirinto *labirinto, int caminho[labirinto->linhas][labirinto->colunas]) {
-    int i, j;
-    printf("\n\n");
-    printf("  ");
-    for (j = 0; j < labirinto->colunas; j++)
-        printf("\t%d ", j);
-
-    for (i = 0; i < labirinto->linhas; i++) {
-        printf("\n\n%d ", i);
-        for (j = 0; j < labirinto->colunas; j++) {
-            printf("\t%d ", caminho[i][j]);
-        }
+    if (movimenta_estudante(estudante, labirinto, analise, caminho, x, y)){
+        return 1;
+    }else{
+        return 0;
     }
 }
