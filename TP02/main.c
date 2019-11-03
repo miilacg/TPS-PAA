b79e8f1058de790434cd6f779c9ab1e1cceeac77
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2019-11-02 22:56:21 -0300

    TP02
    Menu parcial e leitura da piramide

--- TP02/main.c
+++ TP02/main.c
@@ -5,79 +5,143 @@
 //Alunos: Camila C. Guimar√£es - 2256 e Samuel Silva - 2662
 //Trabalho Pratico 02
 
-//#include "partea.c"
+#include "partea.c"
 #include <stdio.h>
 
 int main(){
 	srand(time(NULL)); //usado para funcao rand gerar numeros diferentes em um pequeno tempo
 	
-	int op, leuArquivo, opcao;
+	int op, leuArquivo, opcao, quantidade, opc, solucao;
 	char nomeArquivo[50];
+	TipoPiramide piramide;
 
 	do{
 		//menu para selecao das opcoes
-	    printf ("\n**************************************************************************");
-	    printf ("\n*                                                                        *");
-	    printf ("\n*                      Escolha uma das opcoes abaixo                     *");
-	    printf ("\n*                   1 - Carregar novo arquivo de dados                   *");
-		printf ("\n*                   2 - Sair                                             *");
-	    printf ("\n*                                                                        *");
-	    printf ("\n**************************************************************************");
+	    printf ("\n****************************************************************************************");
+	    printf ("\n*                                                                                      *");
+	    printf ("\n*                            Escolha uma das opcoes abaixo                             *");
+	    printf ("\n*                               1 - Maior soma                                         *");
+	    printf ("\n*                               2 - Distancia de edicao                                *");
+		printf ("\n*                               3 - Sair                                               *");
+	    printf ("\n*                                                                                      *");
+	    printf ("\n****************************************************************************************");
 	    printf ("\n");
         do{
             printf ("Entre com a opcao valida: ");
             scanf ("%d", &op);
-        }while (op < 0 || op > 2); //garante que a opcao de entrada sera valida
+        }while (op < 0 || op > 3); //garante que a opcao de entrada sera valida
         
         switch(op){
-            case 1:
-                printf("Digite o nome do arquivo de texto (sem a extensao): ");
-                scanf("%s", nomeArquivo);
-                leuArquivo = lerArquivo(&piramide, nomeArquivo);
-                if (leuArquivo){ //se o arquivo foi lido com sucesso
-                	system("cls");
-                	printf ("\nArquivo lido com sucesso!\n\n");
-                	
-                	
-                	//menu para selecao das opcoes
-                	printf ("\n**************************************************************************");
-				    printf ("\n*                                                                        *");
-				    printf ("\n*                 Escolha qual forma voce deseja executar                *");
-				    printf ("\n*                         1 - Recursao                                   *");
-				    printf ("\n*                         2 - Memoization                                *");
-				    printf ("\n*                         3 - De tr·s pra frente                         *");
-				    printf ("\n*                         4 - Voltar                                     *");
-				    printf ("\n*                         5 - Sair do programa                           *");
-				    printf ("\n*                                                                        *");
-				    printf ("\n**************************************************************************");
-	
-				    printf ("\n");
-			        do{
-			            printf ("Entre com a opcao valida: ");
-			            scanf ("%d", &opcao);
-			        }while (opcao < 0 || opcao > 5); //garante que a opcao de entrada sera valida
+        	case 1:
+        		system("cls");				
+				//menu para selecao das opcoes
+                printf ("\n****************************************************************************************");
+			    printf ("\n*                                                                                      *");
+			    printf ("\n*               Escolha uma das opcoes abaixo para calcular a maior soma               *");
+			    printf ("\n*                           1 - Carregar um arquivo de dados                           *");
+			    printf ("\n*                           2 - Criar piramides aleatorias                             *");
+			    printf ("\n*                           3 - Voltar                                                 *");
+			    printf ("\n*                           4 - Sair do programa                                       *");
+			    printf ("\n*                                                                                      *");
+			    printf ("\n****************************************************************************************");
+			    printf ("\n");
+		        do{
+		            printf ("Entre com a opcao valida: ");
+		            scanf ("%d", &opcao);
+		        }while (opcao < 0 || opcao > 4); //garante que a opcao de entrada sera valida
 			        
-			        switch(opcao){
-			            case 1:
-			            	system("cls");
-		            	break;
-		            	case 2:
-		            	break;
-		            	case 3:
-		            	break;
-		                case 4:
-		                	//so volta pro menu anterior	
-						break;						
-						case 5:
-							exit(0);
-						break;
-			        }
-            	}else{
-            		printf ("\nEntre com um arquivo valido\n\n");
-				}       
-	        break;
-        }
-    }while(op!=2);   
+		        switch(opcao){
+		            case 1:
+		            	printf("Digite o nome do arquivo de texto (sem a extensao): ");
+		                scanf("%s", nomeArquivo);
+		                leuArquivo = lerArquivo(&piramide, nomeArquivo);
+		                if (leuArquivo){ //se o arquivo foi lido com sucesso
+		                	//system("cls");
+		                	printf ("\nArquivo lido com sucesso!\n\n");
+		                	
+		                	//menu para selecao das opcoes
+			                printf ("\n****************************************************************************************");
+						    printf ("\n*                                                                                      *");
+						    printf ("\n*                     Escolha qual dos algoritmos deseja executar                      *");
+						    printf ("\n*                                 1 - Recursivo                                        *");
+						    printf ("\n*                                 2 - Memoization                                      *");
+						    printf ("\n*                                 3 - Iterativo                                        *");
+						    printf ("\n*                                 4 - Voltar                                           *");
+						    printf ("\n*                                 5 - Sair do programa                                 *");
+						    printf ("\n*                                                                                      *");
+						    printf ("\n****************************************************************************************");
+		
+						    printf ("\n");
+					        do{
+					            printf ("Entre com a opcao valida: ");
+					            scanf ("%d", &opc);
+					        }while (opc < 0 || opc > 5); //garante que a opcao de entrada sera valida
+					        
+					        switch(opc){
+					            case 1:
+					            	/*system("cls");
+				                    solucao =  //chama a funcao que trata de forma recursiva
+				                    if (modoAnalise){//contabilizar as analises
+								    	printf ("\nModo analise ativo\n\n"); 
+										//imprimir tempo de execucao e memoria utilizada
+										//imprimir o valor da soma e o caminho										
+									}else{
+										printf ("\nModo analise nao ativo\n\n");
+										//imprimir o valor da soma e o caminho
+									}*/
+				            	break;
+				            	case 2:
+				            		/*system("cls");
+				                    solucao =  //chama a funcao que trata de forma memoization
+				                    if (modoAnalise){//contabilizar as analises
+								    	printf ("\nModo analise ativo\n\n"); 
+										//imprimir tempo de execucao e memoria utilizada
+										//imprimir o valor da soma e o caminho										
+									}else{
+										printf ("\nModo analise nao ativo\n\n");
+										//imprimir o valor da soma e o caminho
+									}*/
+				            	break;
+				            	case 3:
+				            		/*system("cls");
+				                    solucao =  //chama a funcao que trata de forma iterativa
+				                    if (modoAnalise){//contabilizar as analises
+								    	printf ("\nModo analise ativo\n\n"); 
+										//imprimir tempo de execucao e memoria utilizada
+										//imprimir o valor da soma e o caminho										
+									}else{
+										printf ("\nModo analise nao ativo\n\n");
+										//imprimir o valor da soma e o caminho
+									}*/
+				            	break;
+				            		system ("cls");
+				            	case 4:
+				            	break;
+				                case 5:
+				                	exit(0);	
+								break;						
+					        }
+		            	}
+	            	break;
+	            	case 2:
+			        	/*printf("\n Informe quantas piramides deseja criar: ");
+		                scanf("%d", &quantidade);
+		                if(geraPiramides(linhas, colunas, qtdChaves, qtdPortas, qtdParedes)) //chama a funcao que gera a piramide
+		                	printf ("\nPiramides criadas com sucesso.\n"); */
+			        break;
+			        system ("cls");
+	            	case 3:
+	            	break;
+	                case 4:
+	                	exit(0);	
+					break;						
+		        }
+        	break;
+        	case 2:
+        		//verificar o que È o problema da distancia
+        	break;
+    	}        
+    }while(op!=3);   
 	 
     return 0;
 }
--- TP02/partea.h
+++ TP02/partea.h
@@ -1,10 +1,13 @@
-typedef struct {
-    FILE *arquivo;
-    char nome[100]; 
-}dadosA;
+#define modoDebug 1 //definir como 1 se deseja ter as informações do modoDebug e 0 se não desejar
 
-typedef struct {
-    long int ** piramede;
-    long int maxY;
-    long int maxX;
-}dados;
+typedef struct Piramide{
+    char **espaco;
+    int qtdLinhas;
+}TipoPiramide;
+
+int lerArquivo(TipoPiramide *piramide, char *nomeArquivo);
+void alocaEspaco(TipoPiramide *piramide);
+int contaLinhas(TipoPiramide *piramide, char *nomeArquivo);
+
+//funcoes para testes
+void imprimir(TipoPiramide *piramide);
